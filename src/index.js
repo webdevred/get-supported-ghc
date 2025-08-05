7be73bd404f20f29e6d140f88718934e31951d5f
debbie <148627186+webdevred@users.noreply.github.com>
2025-08-05 14:07:04 +0200

    Added input for package.yaml location

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const { exec } = require("child_process");
 const fs = require("fs");
 const path = require("path");
 const yaml = require("js-yaml");
+const githubCore = require("@actions/core");
 
 async function runCommand(cmd) {
   return new Promise((resolve, reject) => {
@@ -49,8 +50,9 @@ function versionLess(v1, v2) {
 async function main() {
   try {
     const files = fs.readdirSync(process.cwd());
+    const packageYamlPath = githubCore.getInput("package-yaml-path") || path.join(process.cwd(), "package.yaml");
 
-    const baseUpperBound = getBaseUpperBound(path.join(process.cwd(), "package.yaml"));
+    const baseUpperBound = getBaseUpperBound(packageYamlPath);
 
     const ghcupListStr = await runCommand("ghcup list -t ghc -r");
     const lines = ghcupListStr.split("\n").filter(Boolean);
